de022e76fa3d662b80ae43a9f729efd166f26682
Gaurav Bhatti <dw1227@wayne.edu>
2024-08-19 02:09:23 -0400

    Fixed the comment and error message in code/get_rrndb_files.sh. The if/else statement only checks if unzipping was successful

--- code/get_rrndb_files.sh
+++ code/get_rrndb_files.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # author: Gaurav Bhatti
-# inputs: Names of the file to be extracted from the archive (without the path or zip extension)
+# inputs: Names of the file to be extracted from the archive (without
+# the path or zip extension)
 # outputs: The appropriate file into data/raw/
 
 target=$1
@@ -14,14 +15,14 @@ curl -L https://rrndb.umms.med.umich.edu/downloads/"$filename".zip \
 
 unzip -n -d "$path" "$target".zip
 
-# Touch the downloaded files to update time stamps only if the download
-# was successful and the files were unzipped.
+# Touch the downloaded files to update time stamps only if the files
+# were unzipped.
 
  if [[ $? -eq 0 ]]
  then 
      touch "$target" # update time stamp
  else
-     echo "FAIL: Could not download/extract $filename"
+     echo "FAIL: Could not extract $filename"
      exit 1
  fi
 
